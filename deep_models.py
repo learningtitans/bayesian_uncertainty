Fixing training with standard scaling

deep_models.py
@@ -52,45 +52,35 @@ class MLPNormal(BaseEstimator, RegressorMixin):
     
     def fit(self, X, y):
         input_ = Input(shape=(X.shape[-1],))
-        x = Dense(X.shape[-1], activation='relu')(input_)
-        #x = Dropout(0.5)(x)
-        #x = Dense(512, activation='relu')(x)
-        #x = Dropout(0.5)(x)
+        x = Dense(512, activation='relu')(input_)
+        x = Dropout(0.5)(x)
+        x = Dense(512, activation='relu')(x)
+        x = Dropout(0.5)(x)
         mean = Dense(1, activation='linear')(x)
         std = Dense(1, activation=lambda y: K.exp(y))(x)
-        #std = Dense(1, activation='linear')(x)
         out = concatenate([mean, std])
 
         self.model = Model(inputs=[input_], outputs=[out])
 
         def normalLL(yTrue, yPred):
             mean = yPred[:,0]
-            std = yPred[:,1]+1e-5
+            std = yPred[:,1]
             return -K.mean(Normal(mean, std).log_prob(yTrue))
 
-#         def log_gaussian2(x, mean, log_std):
-#             log_var = 2*log_std
-#             return -np.log(2*np.pi)/2.0 - log_var/2.0 - (x-mean)**2/(2*K.exp(log_var))
-
-#         def normalLL(y_true, y_pred):
-#             return -K.mean(log_gaussian2(y_pred[:, 0], y_true[:, 0], y_pred[:, 1]))
-
         self.model.compile(loss=normalLL,
-              optimizer=Adam(lr=1e-4),
+              optimizer=Adam(),
               metrics=['mse'])
 
         self.model.fit(X, y,
                     batch_size=BATCH_SIZE,
                     epochs=N_EPOCHS,
-                    verbose=1)
+                    verbose=0)
         
         return self
 
     def predict(self, X, y=None):
         pred_mean = self.model.predict(X)[:,0]
-        pred_std = self.model.predict(X)[:,1]+1e-5
-        #pred_std = np.exp(self.model.predict(X)[:,1])
-        print(np.mean(pred_mean), np.mean(pred_std))
+        pred_std = self.model.predict(X)[:,1]
         return pred_mean, pred_std
     
     

deep_regression_matrix.py
@@ -1,6 +1,3 @@
-from sklearn.model_selection import cross_validate, ShuffleSplit, KFold, RepeatedKFold
-from sklearn.metrics import make_scorer
-from datetime import datetime
 import numpy as np
 import scipy
 import pickle
@@ -8,57 +5,12 @@ import json
 from itertools import product
 
 from datasets import make_datasets
-from metrics import Results, normal_nll, rmse, mae, auc_rmse, auc_mae
+from metrics import eval_dataset_model, Results, normal_nll, rmse, mae, auc_rmse, auc_mae
 from deep_models import MLPNormal, MLPBaseline, MLPBayesianDropout
 
 datasets = make_datasets(year=False)
 
 models = [MLPNormal, MLPBaseline, MLPBayesianDropout]
-
-
-def eval_dataset_model(d, X, y, model):    
-    try:
-        X = X.values
-        y = y.values
-    except AttributeError:
-        pass
-    
-    if d == 'year':
-        cv = ShuffleSplit(1, test_size=0.1)
-    elif d == 'protein':
-        cv = KFold(n_splits=3)
-    elif d.startswith('make'):
-        cv = KFold(n_splits=5)#ShuffleSplit(1, test_size=0.8)
-    else:
-        cv = RepeatedKFold(n_splits=10, n_repeats=4)
-    
-    reg = model()
-    cv_metrics = []
-    for train_index, test_index in cv.split(X):
-        X_train, X_test = X[train_index], X[test_index]
-        y_train, y_test = y[train_index], y[test_index]
-        reg.fit(X_train, y_train)
-        pred_mean, pred_std = reg.predict(X_test)
-        cv_metrics.append((
-            normal_nll(y_test, pred_mean, pred_std),
-            rmse(y_test, pred_mean),
-            mae(y_test, pred_mean),
-            auc_rmse(y_test, pred_mean, pred_std),
-            auc_mae(y_test, pred_mean, pred_std)))
-
-    metrics_mean = np.mean(cv_metrics, axis = 0)
-    metrics_stderr = scipy.stats.sem(cv_metrics, axis = 0)
-
-    r = Results(
-        str(datetime.now()),
-        d, 
-        model.__name__,
-        X.shape,
-        *zip(metrics_mean, metrics_stderr)
-    )
-    print(r)
-    return r
-
     
 results = [eval_dataset_model(d, X, y, m) for (d, (X, y)), m in product(datasets.items(), models)]
 